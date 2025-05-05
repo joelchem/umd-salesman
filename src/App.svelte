ec2917c80a5500880fced93fe14c7d8441ba9430
Joel Chemmanur <joelchem@outlook.com>
2025-05-05 13:30:41 -0400

    more routing stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
   import AutoComplete from "simple-svelte-autocomplete";
+  import { buildings } from "./routing/buildings";
+  import { tick } from "svelte";
 
-  const colors = ["White", "Red", "Yellow", "Green", "Blue", "Black"];
+  let selectedBuildings: String[] = $state([]);
+  let inpBuilding: String | undefined = $state(undefined);
 </script>
 
 <main class="bg-white h-full w-full flex justify-center">
-  
   <div class="max-w-[40rem] p-8">
     <h1 class="font-bold text-6xl">UMD Salesman</h1>
-    <p class="py-10 text-xl">Find the best route between a set of UMD buildings using different approaches to the Traveling Salesman Problem</p>
-    <h2 class="font-bold text-2xl">Buildings</h2>
+    <p class="py-10 text-xl">
+      Find the best route between a set of UMD buildings using different
+      approaches to the Traveling Salesman Problem
+    </p>
 
-    <ul class="text-lg list-inside">
-      <li>ESJ - Edward St. John Learning Center</li>
-      <li>IRB - Brendan Iribe Center for CS</li>
-    </ul>
-    <AutoComplete items={colors}/>
+    {#if selectedBuildings.length > 0}
+      <h2 class="font-bold text-2xl">Buildings</h2>
+      <ul class="text-lg list-inside">
+        {#each selectedBuildings as building}
+          <li>{building}</li>
+        {/each}
+      </ul>
+    {/if}
+
+    <AutoComplete 
+      items={buildings} 
+      bind:text={inpBuilding}
+      placeholder="Add Building..."
+      onChange={async (e: String) => {
+        if(e) {
+          selectedBuildings.push(e);
+          inpBuilding = "";
+        }
+      }}
+      class="w-32"
+      />
   </div>
-
 </main>
-
---
+++ src/routing/buildings.ts
@@ -0,0 +1,1 @@
+export const buildings = await (await fetch("building-names.json")).json();
